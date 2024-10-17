3ec7707d64a7f43791cfc9fcdf4ad101deb1509e
Aashish NEU <chaple.a@northeastern.edu>
2024-10-17 13:00:37 -0400

    updated app_install.sh

--- packer/scripts/app_install.sh
+++ packer/scripts/app_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Install Node.js and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+cd /opt/my-app || exit
 
 # Install dependencies
 npm install
