d38d8cef93cc87458b3f89805cd253f71001af41
Aashish NEU <chaple.a@northeastern.edu>
2024-10-17 12:50:44 -0400

    updated app_install.sh

--- packer/scripts/app_install.sh
+++ packer/scripts/app_install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-cd /opt/my-app || exit
-
-# Install Node.js and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install dependencies
 npm install
-
-# Set environment variables
-export DB_HOST="localhost" > /opt/my-app/.env
-export DB_USER="myuser" > /opt/my-app/.env
-export DB_PASSWORD="password" > /opt/my-app/.env
-export DB_NAME="cloud_db" > /opt/my-app/.env
