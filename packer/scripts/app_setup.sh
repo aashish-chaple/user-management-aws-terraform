1d23b18e30d5e327fcd10874ce83fdce5a60fbf0
Aashish NEU <chaple.a@northeastern.edu>
2024-10-17 14:29:13 -0400

    updated pkr.hcl file

--- packer/scripts/app_setup.sh
+++ packer/scripts/app_setup.sh
@@ -5,10 +5,10 @@ set -ex
 sudo mkdir -p /opt/myApp
 
 # Unzip the app into the directory
-sudo unzip /tmp/app.zip -d /opt/myApp
+sudo unzip -o /tmp/app.zip -d /opt/myApp
 
 sudo chown -R csye6225:csye6225 /opt/myApp
-sudo chmod 750 /opt/myApp
+sudo chmod -R 775 /opt/myApp
 
 cd /opt/myApp 
 sudo npm install
