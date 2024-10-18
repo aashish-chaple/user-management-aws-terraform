249cdeb9e3a4be27de986e4dea81347c692d407c
Aashish NEU <chaple.a@northeastern.edu>
2024-10-18 19:45:17 -0400

    updated packer

--- packer/scripts/db_setup.sh
+++ packer/scripts/db_setup.sh
@@ -6,6 +6,11 @@ DB_PASS=$3
 
 sudo apt-get install -y postgresql postgresql-contrib
 sudo service postgresql start
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
-sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD $DB_PASS;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+# Create the database
+sudo -u postgres psql -c "CREATE DATABASE \"$DB_NAME\";"
+
+# Create the user
+sudo -u postgres psql -c "CREATE USER \"$DB_USER\" WITH PASSWORD '$DB_PASS';"
+
+# Grant privileges
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE \"$DB_NAME\" TO \"$DB_USER\";"
