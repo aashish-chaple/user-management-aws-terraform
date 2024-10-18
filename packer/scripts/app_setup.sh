ab178d48be0a36e33e9d1e18c51fc64286f35ba7
Aashish NEU <chaple.a@northeastern.edu>
2024-10-18 19:37:21 -0400

    updated packer

--- packer/scripts/app_setup.sh
+++ packer/scripts/app_setup.sh
@@ -3,7 +3,7 @@
 set -ex
 
 curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+sudo apt-get install -y nodejs
 echo 'Node Installed'
 node -v
 npm -v
